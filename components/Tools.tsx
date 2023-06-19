9e9bbd778615446f9e3f6f741c6462dcb7f5f386
RAHUl-KAD <kadamrahulj0909@gmail.com>
2023-06-20 02:17:11 +0530

    updated button UI and updated enhancement image

--- components/Tools.tsx
+++ components/Tools.tsx
@@ -17,8 +17,8 @@ export default function Tools(){
                             {/* <img src="https://ik.imagekit.io/uti9laa1e/tools-enhancement1.png?updatedAt=1687183724987" alt="Tools enhancement" className='rounded-2xl w-full h-72 sm:h-72' /> */}
                             
                             <ReactCompareSlider
-                                itemOne={<ReactCompareSliderImage src="https://ik.imagekit.io/uti9laa1e/oldman.jpg?updatedAt=1687143049080"  alt="Image one" />}
-                                itemTwo={<ReactCompareSliderImage src="https://ik.imagekit.io/uti9laa1e/oldman-new.jpg?updatedAt=1687143051022"  alt="Image two" />}
+                                itemOne={<ReactCompareSliderImage src="https://ik.imagekit.io/uti9laa1e/human1.jpeg?updatedAt=1687207280230"  alt="Image one" />}
+                                itemTwo={<ReactCompareSliderImage src="https://ik.imagekit.io/uti9laa1e/human1-new_1_.jpeg?updatedAt=1687207351548"  alt="Image two" />}
                                 className="sm:w-[300px] h-[300px] rounded-3xl"
                             />
 
@@ -26,7 +26,7 @@ export default function Tools(){
                                 <h5 className="text-2xl font-bold tracking-tight text-gray-900 mt-5">Image Enhancement</h5>
                             </a>
                             <p className="font-normal text-gray-700 sm:mt-10 mt-5">Enhancement your image upto 4x or 8x in seconds, also enhance low light images</p>
-                            <a href='/img-enhancement' className="mt-5 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            <a href='/img-enhancement' className="sm:mt-7 mt-5 text-sm font-medium text-center items-center justify-center rounded-xl focus-visible:outline-black focus:outline-none inline-flex bg-black border-2 border-black duration-200 focus-visible:ring-black hover:bg-transparent hover:border-black hover:text-black lg:w-auto px-6 py-3 text-white">
                                 Enhance Now
                                 <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                             </a>
@@ -45,7 +45,7 @@ export default function Tools(){
                                 <h5 className="text-2xl font-bold tracking-tight text-gray-900 mt-5">Image Background Removal</h5>
                             </a>
                             <p className="font-normal text-gray-700 mt-5">Extract the main object from a picture with incredible accuracy.</p>
-                            <a href='/bg-remove' className="sm:mt-7 mt-5 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            <a href='/bg-remove' className="sm:mt-7 mt-5 text-sm font-medium text-center items-center justify-center rounded-xl focus-visible:outline-black focus:outline-none inline-flex bg-black border-2 border-black duration-200 focus-visible:ring-black hover:bg-transparent hover:border-black hover:text-black lg:w-auto px-6 py-3 text-white">
                                 Remove Bg
                                 <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                             </a>
@@ -63,7 +63,7 @@ export default function Tools(){
                                 <h5 className="text-2xl font-bold tracking-tight text-gray-900 mt-12">Restore Old photos</h5>
                             </a>
                             <p className="font-normal text-gray-700 sm:mt-12 mt-5">Restore your old photos, relive those memories again</p>
-                            <a href='/restore-old/' className="sm:mt-7 mt-5 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            <a href='/restore-old/' className="sm:mt-7 mt-5 text-sm font-medium text-center items-center justify-center rounded-xl focus-visible:outline-black focus:outline-none inline-flex bg-black border-2 border-black duration-200 focus-visible:ring-black hover:bg-transparent hover:border-black hover:text-black lg:w-auto px-6 py-3 text-white">
                                 Restore photos
                                 <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                             </a>
