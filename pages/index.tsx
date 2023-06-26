4d8f83ad43490943db75552e8cf06cc6a84f9443
RAHUl-KAD <kadamrahulj0909@gmail.com>
2023-06-26 22:59:02 +0530

    updated button to forward to background removal

--- pages/index.tsx
+++ pages/index.tsx
@@ -68,9 +68,9 @@ const Home: NextPage = () => {
 
           <Link
             className="items-center mt-7 justify-center font-medium rounded-xl focus-visible:outline-black focus:outline-none inline-flex bg-black border-2 border-black duration-200 focus-visible:ring-black hover:bg-transparent hover:border-black hover:text-black lg:w-auto px-6 py-3 text-center text-white"
-            href="/restore-old#processing"
+            href="/bg-remove#processing"
           >
-            Restore Photos
+            Remove Background
           </Link>
 
           <Link
